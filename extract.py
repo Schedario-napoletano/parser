630f2c2e3cb583eb5f80a595f4894d7a2a18d2bb
Baptiste Fontaine <b@ptistefontaine.fr>
2022-03-24 20:12:01 +0100

    don't write the file until the full list is loaded

--- extract.py
+++ extract.py
@@ -10,8 +10,10 @@ def main():
     p.add_argument("output_path", nargs="?", default="definitions.json")
     opts = p.parse_args()
 
+    definitions = list(parse_definition_dicts(debug=opts.debug))
+
     with open(opts.output_path, "w") as f:
-        json.dump(list(parse_definition_dicts(debug=opts.debug)), f, sort_keys=True, ensure_ascii=False)
+        json.dump(definitions, f, sort_keys=True, ensure_ascii=False)
 
 
 if __name__ == '__main__':
